2dd2eaf1711a072b0028de5845e2715a04b27bc9
leticia2983 <123617103+leticia2983@users.noreply.github.com>
2023-02-13 11:11:44 +0400

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,3 +1,11 @@
+# Linux
+sudo apt-get install python3-venv  # If needed
+python3 -m venv .env
+source .env/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install -r ./requirements.txt
+export set FLASK_APP=hello_app.webapp
+python3 -m flask run
 first=input("First: ")
 second=input("second: ")
 sum=float(first)+float(second)
